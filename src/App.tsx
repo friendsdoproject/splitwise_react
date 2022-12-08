f977afb0c133536e08b4d9c2290461a3b6ba2140
icodeatgit <codeatgit@gmail.com>
2022-12-08 21:58:30 +0530

    Initial changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Splitwise React App
+          Welcome team
         </p>
         <a
           className="App-link"
