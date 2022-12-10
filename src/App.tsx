44986a44d5f1b41e45f89261b69abc94e60c7000
F.R.I.E.N.D.S <friendsdoproject@gmail.com>
2022-12-10 21:21:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Team
+          Welcome Team !!
         </p>
         <ul>
           <li>Anand</li>
