cea1ebba26be056aabebe2c187874108ff58d8b2
icodeatgit <codeatgit@gmail.com>
2022-12-19 22:54:50 +0530

    Login Page initial CSS Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { createContext } from "react";
+export const ThemeContext = createContext<{}>({});
 
 const App = () => {
+  const [theme, setTheme] = useState<string>("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+  theme === "dark"
+    ? (document.body.style.backgroundColor = "#181818")
+    : (document.body.style.backgroundColor = "#dde1e7");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Team !!
-        </p>
-        <ul>
-          <li>Anand</li>
-          <li>Dhayalan</li>
-          <li>Jaison</li>
-          <li>Ilayaraja</li>
-          <li>Aravindhan</li>
-          <li>Kiruthi</li>
-          <li>Arunachalam</li>
-          <li>Jawahar</li>
-          <li>Bincy</li>
-          <li>Jagadesh</li>
-          <li>Bharath</li>
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="content" id={theme}>
+        <div className="text" id={theme}>
+          Sign in
+        </div>
+        <form>
+          <div className="field" id={theme}>
+            <span className="fa fa-user"></span>
+            <input type="text" placeholder="Email Id" id={theme} />
+          </div>
+          <div className="field" id={theme}>
+            <span className="fa fa-lock"></span>
+            <input type="password" id={theme} placeholder="Password" />
+          </div>
+          <button id={theme}>Log in</button>
+        </form>
+        <label className="switch" id={theme}>
+          <input type="checkbox" onClick={toggleTheme} />
+          <span className="slider"></span>
+          <span className="on">ON</span>
+          <span className="off">OFF</span>
+        </label>
+      </div>
+    </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
